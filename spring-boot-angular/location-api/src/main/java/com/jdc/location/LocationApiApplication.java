7c5e314953dce532d2082d8cda8467f49fbcaa2e
minlwin <lwin.zawmin@gmail.com>
2020-08-13 18:55:12 +0630

    Share Spring Boots Application

---
+++ spring-boot-angular/location-api/src/main/java/com/jdc/location/LocationApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-angular/location-db/README.md
@@ -0,0 +1,1 @@
+# Docker Demo for PostgreSQL DB Image
