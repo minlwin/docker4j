ad5cd681cf7f71520ae85465c1e6918fe6aba366
minlwin <lwin.zawmin@gmail.com>
2020-08-17 19:32:01 +0630

    Add Spring API Project

---
+++ spring-boot-angular-proxy/proxy-api/src/main/java/com/jdc/words/ProxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-angular-proxy/proxy-api/src/test/java/com/jdc/words/ProxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
